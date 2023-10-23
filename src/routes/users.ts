2df637c28ef5961debdf31c15732a4a2d276a1dd
tivrusky <kallelr@outlook.com>
2023-10-23 20:09:58 -0300

    feat: criando primeira rota e migration

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,3 +1,33 @@
 import { FastifyInstance } from 'fastify'
 import { knex } from '../database'
 import { z } from 'zod'
+import { randomUUID } from 'crypto'
+
+export async function usersRoutes(app: FastifyInstance) {
+  app.post('/', async (req, res) => {
+    const createBodyUserSchema = z.object({
+      name: z.string(),
+      email: z.string().email(),
+    })
+    const { name, email } = createBodyUserSchema.parse(req.body)
+
+    let sessionId = req.cookies.sessionId
+    if (!sessionId) {
+      sessionId = randomUUID()
+
+      res.cookie('sessionId', sessionId, {
+        path: '/',
+        maxAge: 60 * 60 * 24 * 7, // 7 days
+      })
+    }
+
+    await knex('users').insert({
+      id: randomUUID(),
+      name,
+      email,
+      session_id: sessionId,
+    })
+
+    return res.status(201).send()
+  })
+}
