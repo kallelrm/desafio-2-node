d6e47b7a9f59e876247ed625a2a8416a3060e31a
tivrusky <kallelr@outlook.com>
2023-10-24 21:12:05 -0300

    criando rotas de usuarios

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -8,8 +8,9 @@ export async function usersRoutes(app: FastifyInstance) {
     const createBodyUserSchema = z.object({
       name: z.string(),
       email: z.string().email(),
+      password: z.string(),
     })
-    const { name, email } = createBodyUserSchema.parse(req.body)
+    const { name, email, password } = createBodyUserSchema.parse(req.body)
 
     let sessionId = req.cookies.sessionId
     if (!sessionId) {
@@ -25,9 +26,42 @@ export async function usersRoutes(app: FastifyInstance) {
       id: randomUUID(),
       name,
       email,
+      password,
       session_id: sessionId,
     })
 
     return res.status(201).send()
+  })
+
+  app.post('/login', async (req, res) => {
+    const createSessionUserSchema = z.object({
+      email: z.string(),
+      password: z.string(),
+    })
+
+    const { email, password } = createSessionUserSchema.parse(req.body)
+
+    const [user] = await knex('users').where('email', email).limit(1)
+
+    if (!user) {
+      return res.status(400).send({ message: 'email not found' })
+    }
+
+    const userLogin = user.password === password
+    if (!userLogin) {
+      return res.status(401).send({ message: 'wrong password' })
+    }
+
+    user.session_id = randomUUID()
+
+    res.cookie('sessionId', user.session_id, {
+      path: '/',
+      maxAge: 60 * 60 * 24 * 7, // 7 days
+    })
+    await knex('users').where('email', email).update({
+      session_id: user.session_id,
+    })
+
+    return { session_id: user.session_id }
   })
 }
